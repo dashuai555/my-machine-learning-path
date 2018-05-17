8942b2b554bd589086faa82b680f4b3e3bd8574b
Han.X <xiaohan1992a@163.com>
2018-05-17 22:29:25 +0800

    对kNN进行修改，增加了处理文档中数据的内容

--- __init__.py
+++ __init__.py
@@ -2,6 +2,9 @@
 # -*- coding:utf-8 -*-
 import kNN
 
-group,labels=kNN.createDataSet()
-c=kNN.classify0([0,0],group,labels,3)
-print(c)
+# group,labels=kNN.createDataSet()
+# c=kNN.classify0([0,0],group,labels,3)
+# print(c)
+dataSet,labels=kNN.file2matrix('C:/Users/12076/PycharmProjects/practice/venv/machine_learning/dataset.txt')
+print(dataSet)
+print(labels)
--- kNN.py
+++ kNN.py
@@ -42,3 +42,41 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1), reverse=True)
     # 返回sortedClassCount列表的第0个的第0项
     return sortedClassCount[0][0]
+
+    #############k-近邻算法改进约会网站的配对效果
+    # 从文本中解析数据
+
+def file2matrix(filename):
+    labels={'largeDoses':2,'smallDoses':1,'didntLike':0}
+    # 打开文件
+    fr=open(filename)
+    # readlines() 方法用于读取所有行(直到结束符 EOF)并返回列表，该列表可以由 Python 的 for... in ... 结构进行处理。
+    # 如果碰到结束符 EOF 则返回空字符串。
+    # 返回列表，包含所有的行。
+    # arrayOLines是一个n*1的向量
+    arrayOLines=fr.readlines()
+    # numberOfLines为n
+    numberOfLines=len(arrayOLines)
+    # np.zero()为返回来一个给定形状和类型的用0填充的数组，下面代码的意思就是创建一个n*3的全零矩阵
+    returnMat=np.zeros((numberOfLines,3))
+    # 创建一个标签向量
+    classLabelVector=[]
+    index=0
+    for line in arrayOLines:
+        # strip()用于移除字符串头尾指定的字符（默认为空格），这一步应该是为了清洗数据,截取掉所有的回车字符
+        line=line.strip()
+        # split('char',num) 通过指定分隔符对字符串进行切片，如果参数 num 有指定值，则仅分隔 num 个子字符串
+        # 这一句将每行字符串分割为一个1*4的列表
+        listFromLine=line.split('\t')
+        listFromLine[-1]=labels[listFromLine[-1]]
+        # 将returnMat的每一行赋予listFromLine的前三个值，classLabelVector添加第四个值
+        returnMat[index,:]=listFromLine[:3]
+        classLabelVector.append(np.int(listFromLine[-1]))
+        # index+1
+        index=index+1
+    return returnMat,classLabelVector
+
+
+
+
+
